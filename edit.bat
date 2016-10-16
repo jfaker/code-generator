f6af3447e27cc9a93d3156d8de604a2043271366
xuesong123 <xuesong.net@gmail.com>
2016-10-17 00:09:27 +0800

    1. 删除无用文件

--- edit.bat
+++
@@ -1,4 +0,0 @@
-"D:\Program Files\Editplus 2\EDITPLUS.EXE" "webapp\h5\test2.html"
-"D:\Program Files\Editplus 2\EDITPLUS.EXE" "webapp\h5\js\jupload.js"
-"D:\Program Files\Editplus 2\EDITPLUS.EXE" "webapp\h5\flash\MainSprite.as"
-"D:\Program Files\Editplus 2\EDITPLUS.EXE" "webapp\h5\css\upload.css"
